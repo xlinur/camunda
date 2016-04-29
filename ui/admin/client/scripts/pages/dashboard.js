262fbefa195ebc17973319165acacc6d1d09069c
Valentin Vago <zeropaper@irata.ch>
2016-04-29 08:22:23 +0200

    improve(admin): add dashboard page

---
+++ ui/admin/client/scripts/pages/dashboard.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var fs = require('fs');
+
+var template = fs.readFileSync(__dirname + '/dashboard.html', 'utf8');
+
+var angular = require('camunda-commons-ui/vendor/angular');
+
+var Controller = ['$scope', 'page', function ($scope, page) {
+
+  $scope.$root.showBreadcrumbs = false;
+
+  page.titleSet('Dashboard');
+
+  page.breadcrumbsClear();
+}];
+
+module.exports = [ '$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {
+    template: template,
+    controller: Controller,
+    authentication: 'required',
+    reloadOnSearch: false
+  });
+}];
