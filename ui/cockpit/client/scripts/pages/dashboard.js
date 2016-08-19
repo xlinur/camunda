92e589b9a4e29bca3fab523f1738c18b10943c0b
Valentin Vago <zeropaper@irata.ch>
2016-08-19 11:20:18 +0200

    feat(dashboard-time-range): add a form to select time period and sampling
    for metrics
    
    Related to CAM-6522

--- ui/cockpit/client/scripts/pages/dashboard.js
+++ ui/cockpit/client/scripts/pages/dashboard.js
@@ -2,6 +2,17 @@
 
 var fs = require('fs');
 var template = fs.readFileSync(__dirname + '/dashboard.html', 'utf8');
+var moment = require('moment');
+
+
+
+function mockData(opts, done) {
+
+  setTimeout(function() {
+    done(null, {});
+  }, 100);
+}
+
 
 var Controller = [
   '$scope',
@@ -14,7 +25,6 @@ var Controller = [
   Views,
   page
 ) {
-    var $rootScope = $scope.$root;
     $scope.mainPlugins = [];
     $scope.miscPlugins = [];
 
@@ -37,12 +47,119 @@ var Controller = [
     $scope.deprecateDashboardProviders = Views.getProviders({ component: 'cockpit.dashboard'});
 
 
-    $rootScope.showBreadcrumbs = true;
+    // $scope.$root.showBreadcrumbs = true;
 
   // reset breadcrumbs
     page.breadcrumbsClear();
 
     page.titleSet('Dashboard');
+
+  // ----------------------------------------------------------------------------------------
+    var dateFormat = 'YYYY-MM-DD HH:mm';
+
+    $scope.selection = {
+      start: moment().format(dateFormat),
+      amount: 15,
+      unit: 'seconds',
+      samples: 200
+    };
+
+    $scope.durationEl = {
+      min: 15,
+      max: 45,
+      step: 15
+    };
+
+    $scope.display = {
+      start: $scope.selection.start,
+      end:  $scope.selection.start,
+      diff: null
+    };
+
+    function updateDisplay() {
+      var sel = $scope.selection;
+      console.info('updateDisplay');//es-lint-disable-line
+      var recs = sel.samples;
+      var start = moment(sel.start, dateFormat);
+      var end = start.clone().subtract(recs * sel.amount, sel.unit);
+
+      $scope.display.start = sel.start;
+      $scope.display.end = end.format(dateFormat);
+
+      $scope.display.diff = moment.duration(end, start).humanize();
+    }
+
+    var mappings = {
+      seconds: {
+        min: 15,
+        max: 45,
+        step: 15
+      },
+      minutes: {
+        min: 1,
+        max: 59,
+        step: 1
+      },
+      hours: {
+        min: 1,
+        max: 23,
+        step: 1
+      },
+      days: {
+        min: 1,
+        max: 6,
+        step: 1
+      },
+      weeks: {
+        min: 1,
+        max: 3,
+        step: 1
+      },
+      months: {
+        min: 1,
+        max: 11,
+        step: 1
+      },
+      years: {
+        min: 1,
+        max: 10,
+        step: 1
+      }
+    };
+    function updateDuration() {
+      var sel = $scope.selection;
+      var el = $scope.durationEl;
+      var map = mappings[sel.unit];
+      console.info('updateDuration');//es-lint-disable-line
+
+      el.step = map.step || 1;
+
+      el.min = map.min || 1;
+      if (sel.duration < el.min || !sel.amount) {
+        console.info('smaller (or missing) than', sel.amount, el.min);//es-lint-disable-line
+        $scope.selection.amount = el.min;
+        $scope.timeRange.amount.$setViewValue(el.min);
+      }
+
+      el.max = map.max || 1;
+      if (sel.duration > el.max) {
+        console.info('bigger than', sel.duration, el.max);//es-lint-disable-line
+        $scope.selection.amount = el.max;
+        $scope.timeRange.amount.$setViewValue(el.max);
+      }
+
+      console.info('updateDuration...', $scope.selection.amount,//es-lint-disable-line
+        $scope.timeRange ? $scope.timeRange.amount.$viewValue : null,//es-lint-disable-line
+        $scope.timeRange ? $scope.timeRange.amount.$modelValue : null);//es-lint-disable-line
+
+      mockData({}, function(err, data) {
+        if (err) { return; }
+        console.info('data', data);//es-lint-disable-line
+        updateDisplay();
+      });
+    }
+    $scope.updateDuration = updateDuration;
+    updateDuration();
   }];
 
 var RouteConfig = [ '$routeProvider', function($routeProvider) {
